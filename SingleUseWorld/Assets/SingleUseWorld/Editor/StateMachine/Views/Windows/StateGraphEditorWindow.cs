0b7ca382be9af9726bf793734115d60fd16cde52
SplineFox <89731027+SplineFox@users.noreply.github.com>
2021-10-17 19:54:01 +0900

    feat: Add state machine editor window

---
+++ SingleUseWorld/Assets/SingleUseWorld/Editor/StateMachine/Views/Windows/StateGraphEditorWindow.cs
@@ -0,0 +1,101 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using System.Linq;
+using SingleUseWorld.StateMachine.Views;
+using SingleUseWorld.StateMachine.Models;
+
+namespace SingleUseWorld.StateMachine.Windows
+{
+    public sealed class StateGraphEditorWindow : EditorWindow
+    {
+        #region Fields
+        private GraphModel _graphModel;
+        private StateGraphView _graphView;
+        private InspectorView _inspectorView;
+        #endregion
+
+        #region Public Methods
+        public void SetCurrentSelection(GraphModel graphModel)
+        {
+
+            if (_graphModel != graphModel)
+            {
+                _graphModel = graphModel;
+                _graphView.SetModel(_graphModel);
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        private void CreateGUI()
+        {
+            InitUxml();
+            InitUss();
+            InitElements();
+            OnSelectionChange();
+        }
+
+        private void OnSelectionChange()
+        {
+            GraphModel selectedAsset = Selection.activeObject as GraphModel;
+            if (selectedAsset != null)
+            {
+                SetCurrentSelection(selectedAsset);
+            }
+        }
+
+        private void InitUss()
+        {
+            var uss = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/SingleUseWorld/Editor/StateMachine/Views/USS/StateGraphEditorWindow.uss");
+            rootVisualElement.styleSheets.Add(uss);
+        }
+
+        private void InitUxml()
+        {
+            var uxml = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/SingleUseWorld/Editor/StateMachine/Views/UXML/StateGraphEditorWindow.uxml");
+            uxml.CloneTree(rootVisualElement);
+        }
+
+        private void InitElements()
+        {
+            _graphView = rootVisualElement.Q<StateGraphView>();
+            _inspectorView = rootVisualElement.Q<InspectorView>();
+        }
+        #endregion
+
+
+        #region Static Methods
+        [MenuItem("SingleUseWorld/Window/StateGraphEditor")]
+        public static void ShowEditorWindow()
+        {
+            FindOrCreateEditorWindow();
+        }
+
+        public static StateGraphEditorWindow FindEditorWindow()
+        {
+            var editorWindow = Resources.FindObjectsOfTypeAll<StateGraphEditorWindow>().FirstOrDefault();
+            return editorWindow;
+        }
+
+        public static StateGraphEditorWindow CreateEditorWindow()
+        {
+            var editorWindow = CreateInstance<StateGraphEditorWindow>();
+            return editorWindow;
+        }
+
+        public static StateGraphEditorWindow FindOrCreateEditorWindow()
+        {
+            var editorWindow = FindEditorWindow();
+            if (editorWindow == null)
+            {
+                editorWindow = CreateEditorWindow();
+            }
+            editorWindow.Show();
+            editorWindow.Focus();
+            return editorWindow;
+        }
+        #endregion
+    }
+}
