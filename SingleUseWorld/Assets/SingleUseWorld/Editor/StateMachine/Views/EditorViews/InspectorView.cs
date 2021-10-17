e904b0f54da7402d0c47220ed54193f00e1049f5
SplineFox <89731027+SplineFox@users.noreply.github.com>
2021-10-17 19:11:13 +0900

    feat: Add state machine editor views

---
+++ SingleUseWorld/Assets/SingleUseWorld/Editor/StateMachine/Views/EditorViews/InspectorView.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SingleUseWorld.StateMachine.Views
+{
+    public class InspectorView : VisualElement
+    {
+        #region Nested Classes
+        public new class UxmlFactory : UxmlFactory<InspectorView, VisualElement.UxmlTraits> { }
+        #endregion
+    }
+}
