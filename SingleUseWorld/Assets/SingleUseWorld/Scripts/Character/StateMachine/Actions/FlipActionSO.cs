8b706ad6670c631438ac598290434cbe0b27cbf2
SplineFox <89731027+SplineFox@users.noreply.github.com>
2021-11-08 09:55:52 +0900

    feat: Add action to flip character sprite when moving

---
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Character/StateMachine/Actions/FlipActionSO.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using SingleUseWorld.StateMachine.Runtime;
+using SingleUseWorld.StateMachine.Edittime;
+
+namespace SingleUseWorld
+{
+    [CreateAssetMenu(fileName = "New FlipAction", menuName = "SingleUseWorld/StateMachine/Character/Actions/Create Flip Action")]
+    public class FlipActionSO : ActionModel<FlipAction> { }
+
+    public class FlipAction : Action
+    {
+        private CharacterInput _characterInput;
+        private SpriteRenderer _spriteRenderer;
+
+        public override void OnInitState(StateRunner stateRunner)
+        {
+            _characterInput = stateRunner.GetComponent<CharacterInput>();
+            _spriteRenderer = stateRunner.GetComponent<SpriteRenderer>();
+        }
+
+        public override void Perform()
+        {
+            if(_characterInput.MoveInput != Vector2.zero)
+            {
+                _spriteRenderer.flipX = _characterInput.MoveInput.x < 0;
+            }
+        }
+    }
+}
