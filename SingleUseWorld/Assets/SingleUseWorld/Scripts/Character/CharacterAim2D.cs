ea6f37fe436bc919e47afb4403e8231c7d8186ee
SplineFox <89731027+SplineFox@users.noreply.github.com>
2021-11-12 15:02:18 +0900

    feat: Add ability to move camera in aim direction

--- SingleUseWorld/Assets/SingleUseWorld/Scripts/Character/CharacterAim2D.cs
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Character/CharacterAim2D.cs
@@ -5,16 +5,32 @@ namespace SingleUseWorld
 {
     public class CharacterAim2D : MonoBehaviour
     {
+        #region Constants
+        private string CAMERA_TARGET_NAME = "CameraTarget";
+        #endregion
+
         #region Fields
         private Camera _mainCamera = default;
+        private GameObject _cameraTarget = default;
         private CharacterInput _characterInput = default;
 
         [SerializeField] private GameObject _crosshair;
+        [SerializeField] private Vector3 _crosshairPosition;
         [SerializeField] private Vector2 _aimDirection;
         [SerializeField] private float _aimAngle;
+
+        [SerializeField] private bool _moveCameraTarget = true;
+        [SerializeField] private float _cameraTargetSpeed = 5f;
+        [SerializeField] private float _cameraTargetDistance = 10f;
+        
+        private Vector3 _newCameraTargetPosition;
+        private Vector3 _newCameraTargetDirection;
         #endregion
 
         #region Properties
+        public GameObject CameraTarget { get => _cameraTarget; }
+        public Vector2 AimDirection { get => _aimDirection; }
+        public float AimAngle { get => _aimAngle; }
         #endregion
 
         #region LifeCycle Methods
@@ -27,6 +43,7 @@ namespace SingleUseWorld
         private void Update()
         {
             UpdateMouseAim();
+            UpdateCameraTarget();
         }
         #endregion
 
@@ -36,6 +53,19 @@ namespace SingleUseWorld
             _mainCamera = Camera.main;
             _characterInput = GetComponent<CharacterInput>();
             Assert.IsNotNull(_characterInput, "\"CharacterInput\" is required.");
+
+            _cameraTarget = transform.Find(CAMERA_TARGET_NAME)?.gameObject;
+            if (_cameraTarget == null) 
+                _cameraTarget = CreateCameraTarget();
+        }
+
+        private GameObject CreateCameraTarget()
+        {
+            var cameraTarget = new GameObject();
+            cameraTarget.transform.SetParent(this.transform);
+            cameraTarget.transform.localPosition = Vector3.zero;
+            cameraTarget.name = CAMERA_TARGET_NAME;
+            return cameraTarget;
         }
 
         private void UpdateMouseAim()
@@ -43,15 +73,33 @@ namespace SingleUseWorld
             Vector2 mouseScreenPosition = _characterInput.LookInput;
             Vector3 mouseWolrdPosition = _mainCamera.ScreenToWorldPoint(mouseScreenPosition);
             mouseWolrdPosition.z = transform.position.z;
-           
-            _aimDirection = mouseWolrdPosition - transform.position;
-            _aimAngle = Mathf.Atan2(_aimDirection.y, _aimDirection.x) * Mathf.Rad2Deg;
 
-            Quaternion angle = Quaternion.Euler(0, 0, _aimAngle);
+            _crosshairPosition = mouseWolrdPosition;
+            _aimDirection = _crosshairPosition - transform.position;
+            _aimAngle = Mathf.Atan2(AimDirection.y, AimDirection.x) * Mathf.Rad2Deg;
+
+            Quaternion angle = Quaternion.Euler(0, 0, AimAngle);
             Vector3 rotation = angle * Vector3.up;
 
-            _crosshair.transform.position = mouseWolrdPosition;
+            _crosshair.transform.position = _crosshairPosition;
             _crosshair.transform.localRotation = angle;
+        }
+
+        private void UpdateCameraTarget()
+        {
+            if (!_moveCameraTarget)
+                return;
+
+            _newCameraTargetPosition = _crosshairPosition;
+            _newCameraTargetDirection = _newCameraTargetPosition - this.transform.position;
+            if(_newCameraTargetDirection.magnitude > _cameraTargetDistance)
+            {
+                _newCameraTargetDirection = _newCameraTargetDirection.normalized * _cameraTargetDistance;
+            }
+            _newCameraTargetPosition = this.transform.position + _newCameraTargetDirection;
+            _newCameraTargetPosition = Vector3.Lerp(_cameraTarget.transform.position, _newCameraTargetPosition, Time.deltaTime * _cameraTargetSpeed);
+            
+            _cameraTarget.transform.position = _newCameraTargetPosition;
         }
         #endregion
     }
