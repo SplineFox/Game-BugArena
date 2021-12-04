87a68dabfb44241614b27d9fe16d262c7441f49a
SplineFox <89731027+SplineFox@users.noreply.github.com>
2021-12-04 17:07:58 +0900

    feat: Set up character aiming

--- SingleUseWorld/Assets/SingleUseWorld/Scripts/Character/CharacterMovement.cs
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Character/CharacterMovement.cs
@@ -9,8 +9,8 @@ namespace SingleUseWorld
     {
         #region Fields
         private Rigidbody2D _rigidbody2D;
+        private Vector2 _velocity;
 
-        [SerializeField] private Vector2Variable _moveInput;
         [SerializeField] private FloatConstant _moveSpeed;
         #endregion
 
@@ -22,10 +22,17 @@ namespace SingleUseWorld
 
         private void FixedUpdate()
         {
-            Vector2 velocity = _moveInput.Value * _moveSpeed.Value;
-            Vector2 translation = velocity * Time.fixedDeltaTime;
+            Vector2 translation = _velocity * Time.fixedDeltaTime;
             Vector2 position = _rigidbody2D.position + translation;
             _rigidbody2D.MovePosition(position);
+        }
+        #endregion
+
+        #region Public Methods
+        public void CalculateVelocity(Vector2 moveInput)
+        {
+            Vector2 direction = moveInput;
+            _velocity = direction * _moveSpeed.Value;
         }
         #endregion
 
@@ -34,7 +41,6 @@ namespace SingleUseWorld
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
             Assert.IsNotNull(_rigidbody2D, "\"Rigidbody2D\" is required.");
-            Assert.IsNotNull(_moveInput, "\"MoveInput Variable\" is required.");
             Assert.IsNotNull(_moveSpeed, "\"MoveSpeed Constant\" is required.");
         }
         #endregion
