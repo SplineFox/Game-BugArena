0b229f8acd1cae84e36dd433fb27168bea83e660
SplineFox <89731027+SplineFox@users.noreply.github.com>
2022-01-27 14:22:56 +0900

    feat: Add projectile shadow subview

---
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Ball/Subviews/ShadowSubview.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace SingleUseWorld
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class ShadowSubview : MonoBehaviour
+    {
+        #region Fields
+        private SpriteRenderer _spriteRenderer;
+        
+        [Min(0f)] [SerializeField] private float _maxVisibleHeight = 1f;
+        [Range(0f, 1f)] [SerializeField] private float _minTransparency = 1f;
+        [Range(0f, 1f)] [SerializeField] private float _maxTransparency = 0.2f;
+        #endregion
+
+        #region LifeCycle Methods
+        private void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        #endregion
+
+        #region Public Methods
+        public void UpdateHeightPresentation(float height)
+        {
+            var normalizedHeight = Mathf.InverseLerp(0f, _maxVisibleHeight, height);
+            UpdateScale(normalizedHeight);
+            UpdateTransparency(normalizedHeight);
+        }
+        #endregion
+
+        #region Private Methods
+        private void UpdateScale(float normalizedHeight)
+        {
+            var newScale = Vector3.one * (1f - normalizedHeight);
+            transform.localScale = newScale;
+        }
+
+        private void UpdateTransparency(float normalizedHeight)
+        {
+            var transparency = Mathf.Lerp(_minTransparency, _maxTransparency, normalizedHeight);
+            var newColor = _spriteRenderer.color;
+            newColor.a = transparency;
+            _spriteRenderer.color = newColor;
+        }
+        #endregion
+    }
+}
