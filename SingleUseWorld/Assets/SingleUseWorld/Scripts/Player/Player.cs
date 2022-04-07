d000e62edce64c80e8432de4111093a02d67056e
SplineFox <89731027+SplineFox@users.noreply.github.com>
2022-04-07 22:05:24 +0900

    feat: Setup components and integrate with animation logic into Player class

--- SingleUseWorld/Assets/SingleUseWorld/Scripts/Player/Player.cs
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Player/Player.cs
@@ -1,4 +1,5 @@
 using SingleUseWorld.FSM;
+using System;
 using UnityEngine;
 
 
@@ -9,8 +10,10 @@ namespace SingleUseWorld
         #region Fields
         [SerializeField] private PlayerBodyView _body = default;
         [SerializeField] private ShadowView _shadow = default;
+        
+        [SerializeField] private PlayerArmament _armament = default;
+        [SerializeField] private PlayerMovement _movement = default;
 
-        private PlayerMovement _movement = default;
         private Projectile2D _projectile2D = default;
         #endregion
 
@@ -18,15 +21,22 @@ namespace SingleUseWorld
         public void Initialize()
         {
             _projectile2D = GetComponent<Projectile2D>();
-            _movement = new PlayerMovement(_projectile2D);
-            _movement.SetSpeed(3.5f);
 
+            _armament.Initialize(this);
+            _armament.StateChanged += OnArmamentStateChanged;
+
+            _movement.Initialize(this);
             _movement.StateChanged += OnMovementStateChanged;
+            
+            _movement.SetSpeed(4f);
+
+            _body.ThrowStartFrameReached += OnThrowStartFrameReached;
+            _body.ThrowEndFrameReached += OnThrowEndFrameReached;
         }
 
         void IControllable.StartMovement()
         {
-            _movement.Start();
+            _movement.StartMovement();
         }
 
         void IControllable.SetMovementDirection(Vector2 direction)
@@ -37,22 +47,80 @@ namespace SingleUseWorld
 
         void IControllable.StopMovement()
         {
-            _movement.Stop();
+            _movement.SetDirection(Vector2.zero);
+            _movement.StopMovement();
         }
 
         void IControllable.Use()
         {
+            if (_armament.State == ArmamentState.Unarmed)
+                return;
+
+            _movement.MovementAllowed = false;
+            _body.PlayThrowAnimation();
         }
 
         void IControllable.Drop()
         {
+            _armament.Drop();
         }
         #endregion
 
         #region Private Methods
-        private void OnMovementStateChanged(MovementState state)
+        private void OnThrowStartFrameReached()
         {
-            switch (state)
+            _armament.Use();
+        }
+
+        private void OnThrowEndFrameReached()
+        {
+            _movement.MovementAllowed = true;
+            _armament.FinishThrowedState();
+        }
+
+        private void OnMovementStateChanged(MovementState movementState)
+        {
+            switch (movementState)
+            {
+                case MovementState.Knocked:
+                    _body.PlayKnockedAnimation();
+                    _armament.Drop();
+                    _armament.PickupAllowed = false;
+                    break;
+                default:
+                    ResolveAnimation();
+                    break;
+            }
+        }
+
+        private void OnArmamentStateChanged(ArmamentState armamentState)
+        {
+            switch (armamentState)
+            {
+                case ArmamentState.Throwed:
+                    break;
+                default:
+                    ResolveAnimation();
+                    break;
+            }
+        }
+
+        private void ResolveAnimation()
+        {
+            switch (_armament.State)
+            {
+                case ArmamentState.Unarmed:
+                    ResolveUnarmedMovement();
+                    break;
+                case ArmamentState.Armed:
+                    ResolveArmedMovement();
+                    break;
+            }
+        }
+
+        private void ResolveUnarmedMovement()
+        {
+            switch (_movement.State)
             {
                 case MovementState.Idling:
                     _body.PlayIdleUnarmedAnimation();
@@ -60,8 +128,18 @@ namespace SingleUseWorld
                 case MovementState.Moving:
                     _body.PlayMoveUnarmedAnimation();
                     break;
-                case MovementState.Knocked:
-                    _body.PlayThrowAnimation();
+            }
+        }
+
+        private void ResolveArmedMovement()
+        {
+            switch (_movement.State)
+            {
+                case MovementState.Idling:
+                    _body.PlayIdleArmedAnimation();
+                    break;
+                case MovementState.Moving:
+                    _body.PlayMoveArmedAnimation();
                     break;
             }
         }
