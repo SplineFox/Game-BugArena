11a9ffbfd58840fb725abd8534cdd060bd4da53e
SplineFox <89731027+SplineFox@users.noreply.github.com>
2022-05-18 10:19:29 +0900

    feat: Add attach and detach animations

--- SingleUseWorld/Assets/SingleUseWorld/Scripts/Item/Item.cs
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Item/Item.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace SingleUseWorld
@@ -5,10 +6,10 @@ namespace SingleUseWorld
     public abstract class Item : MonoBehaviour
     {
         #region Fields
-        private Elevator _elevator = default;
-        private Projectile2D _projectile2D = default;
-        private Rigidbody2D _rigidbody2D = default;
-        private Collider2D _collider2D = default;
+        protected Elevator _elevator = default;
+        protected Collider2D _collider2D = default;
+        protected Rigidbody2D _rigidbody2D = default;
+        protected Projectile2D _projectile2D = default;
         #endregion
 
         #region LifeCycle Methods
@@ -16,42 +17,74 @@ namespace SingleUseWorld
         {
             _elevator = GetComponent<Elevator>();
             _collider2D = GetComponent<Collider2D>();
-            _projectile2D = GetComponent<Projectile2D>();
-
             _rigidbody2D = GetComponent<Rigidbody2D>();
+            _projectile2D = GetComponent<Projectile2D>();
         }
         #endregion
 
         #region Public Methods
         public void Attach(Transform target, float height)
         {
+            // attach to parent
             transform.SetParent(target);
-            transform.Reset();
 
-            _elevator.height = height;
-            _collider2D.enabled = false;
+            // play raise animation
+            StopAllCoroutines();
+            StartCoroutine(MoveTo(Vector3.zero));
+            StartCoroutine(ElevateTo(height));
 
+            // disable physics
             _projectile2D.ResetVelocity();
             _projectile2D.IsKinematic = true;
             _projectile2D.enabled = false;
 
+            // disable collisions
+            _collider2D.enabled = false;
             _rigidbody2D.isKinematic = true;
         }
 
         public void Detach()
         {
+            // detach from parent
             transform.SetParent(null);
 
-            _elevator.height = 0f;
-            _collider2D.enabled = true;
+            // play lower animation
+            StopAllCoroutines();
+            StartCoroutine(ElevateTo(0f));
 
+            // enable physics
             _projectile2D.IsKinematic = false;
             _projectile2D.enabled = true;
 
+            // enable collisions
+            _collider2D.enabled = true;
             _rigidbody2D.isKinematic = false;
         }
 
         public abstract void Use(Vector2 direction);
+        #endregion
+
+        #region Private Methods
+        private IEnumerator ElevateTo(float targetHeight)
+        {
+            targetHeight = Mathf.Max(targetHeight, 0f);
+            while(Mathf.Abs(_elevator.height - targetHeight) > 0.05f)
+            {
+                _elevator.height = Mathf.Lerp(_elevator.height, targetHeight, Time.deltaTime * 10f);
+                yield return null;
+            }
+            _elevator.height = targetHeight;
+        }
+
+        private IEnumerator MoveTo(Vector3 targetPosition)
+        {
+            while(Vector3.Distance(transform.localPosition, targetPosition) > 0.05f)
+            {
+                transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * 10f);
+                yield return null;
+            }
+            transform.localPosition = targetPosition;
+        }
         #endregion
     }
 }
