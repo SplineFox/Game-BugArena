ba5857a36f9976807e856b55f6c5acba3a933b9c
SplineFox <89731027+SplineFox@users.noreply.github.com>
2022-09-02 13:31:03 +0900

    feat: Add MonoFactory for creating GameObject instances

---
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Core/Management/MonoFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace SingleUseWorld
+{
+    public abstract class MonoFactory<TPrefab> : ScriptableObject where TPrefab : MonoBehaviour
+    {
+        #region Fields
+        [SerializeField] protected TPrefab _prefab;
+        #endregion
+
+        #region Public Methods
+        public abstract TPrefab Create();
+
+        public abstract void Destroy(TPrefab instance);
+        #endregion
+
+        #region Protected Methods
+        protected TPrefab CreateInstance()
+        {
+            TPrefab instance = Instantiate(_prefab);
+            return instance;
+        }
+
+        protected void DestroyInstance(TPrefab instance)
+        {
+            Destroy(instance.gameObject);
+        }
+        #endregion
+    }
+}
