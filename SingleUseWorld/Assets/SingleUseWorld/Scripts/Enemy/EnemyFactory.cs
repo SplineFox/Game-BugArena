d027dc6595b9075e7727b2134f552ed2ab7b4826
SplineFox <89731027+SplineFox@users.noreply.github.com>
2022-09-09 14:26:12 +0900

    feat: Add Player and Enemy factories

---
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Enemy/EnemyFactory.cs
@@ -0,0 +1,22 @@
+﻿using SingleUseWorld.FSM;
+using System;
+using UnityEngine;
+
+namespace SingleUseWorld
+{
+    [CreateAssetMenu(fileName = "EnemyFactorySO", menuName = "SingleUseWorld/Factories/Enemies/Enemy Factory SO")]
+    public sealed class EnemyFactory : MonoFactory<Enemy>
+    {
+        #region Public Methods
+        protected override void OnAfterCreate(Enemy instance)
+        {
+            instance.Initialize();
+        }
+
+        protected override void OnBeforeDestroy(Enemy instance)
+        {
+            instance.Deinitialize();
+        }
+        #endregion
+    }
+}
---
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Player/PlayerFactory.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SingleUseWorld
+{
+    [CreateAssetMenu(fileName = "PlayerFactorySO", menuName = "SingleUseWorld/Factories/Player/Player Factory SO")]
+    public class PlayerFactory : MonoFactory<Player>
+    {
+        protected override void OnAfterCreate(Player instance)
+        {
+            instance.Initialize();
+        }
+
+        protected override void OnBeforeDestroy(Player instance)
+        {
+            instance.Deinitialize();
+        }
+    }
+}
