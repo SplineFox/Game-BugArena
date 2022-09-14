0b8a2bc704f24bdbdcd8dd64c86e157b6bd7f26d
SplineFox <89731027+SplineFox@users.noreply.github.com>
2022-09-14 12:01:06 +0900

    feat: Add a Game class that acts as a scene entry point

---
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Core/Management/Game.cs
@@ -0,0 +1,65 @@
+using Cinemachine;
+using UnityEngine;
+
+namespace SingleUseWorld
+{
+    public class Game : MonoBehaviour
+    {
+        #region Fields
+        [SerializeField] private Camera _camera = default;
+        [SerializeField] private CinemachineVirtualCamera _virtualCamera = default;
+        [Space]
+        [SerializeField] private PlayerInput _playerInput = default;
+        [SerializeField] private PlayerFactory _playerFactory = default;
+        [Space]
+        [SerializeField] private SkullItemFactory _skullItemFactory = default;
+        [SerializeField] private SkullEntityFactory _skullProjectileFactory = default;
+
+        private Player _player;
+        private PlayerController _playerController;
+        private CameraController _cameraController;
+        private TargetController _targetController;
+        #endregion
+
+        #region LifeCycle Methods
+        private void Start()
+        {
+            Initialize();
+        }
+
+        private void OnDestroy()
+        {
+            Deinitialize();
+        }
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Internal Methods
+        #endregion
+
+        #region Protected Methods
+        #endregion
+
+        #region Private Methods
+        private void Initialize()
+        {
+            _player = _playerFactory.Create();
+            _playerController = new PlayerController();
+            _cameraController = new CameraController(_camera, _virtualCamera);
+            _targetController = new TargetController(_player.transform, _cameraController);
+
+            _playerController.Initialize(_playerInput, _player, _cameraController, _targetController);
+
+            var item = _skullItemFactory.Create();
+            item.transform.position = _player.transform.position + Vector3.right * 5;
+        }
+
+        private void Deinitialize()
+        {
+            GameObject.Destroy(_player);
+        }
+        #endregion
+    }
+}
