b50e4aefb7b95b1527a7dd71ca3e55695007e577
SplineFox <89731027+SplineFox@users.noreply.github.com>
2022-09-15 15:22:42 +0900

    feat: Deal damage and play animations when SkullEntity hits an enemy

--- SingleUseWorld/Assets/SingleUseWorld/Scripts/Item/SkullEntity.cs
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Item/SkullEntity.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace SingleUseWorld
 {
     public class SkullEntity : ItemEntity
     {
         #region Fields
+        [SerializeField] private SkullEntityView _view = default;
+        [SerializeField] private ShadowView _shadow = default;
         [SerializeField] private ProjectileTrigger _projectileTrigger = default;
 
         private Projectile2D _projectile;
@@ -23,17 +26,22 @@ namespace SingleUseWorld
         {
             base.Awake();
             _projectile = GetComponent<Projectile2D>();
+            Assert.IsNotNull(_projectileTrigger, "SkullEntity: ProjectileTrigger is not assigned!");
+            Assert.IsNotNull(_view, "SkullEntity: SkullEntityView is not assigned!");
+            Assert.IsNotNull(_shadow, "SkullEntity: ShadowView is not assigned!");
         }
 
         public void OnCreate(SkullEntitySettings settings)
         {
             _settings = settings;
             _projectileTrigger.EnemyHit += OnEnemyHit;
+            _projectile.GroundCollision += OnGroundHit;
         }
 
         public void OnDestroy()
         {
             _projectileTrigger.EnemyHit -= OnEnemyHit;
+            _projectile.GroundCollision -= OnGroundHit;
         }
         #endregion
 
@@ -54,6 +62,27 @@ namespace SingleUseWorld
         #region Private Methods
         private void OnEnemyHit(Enemy enemy)
         {
+            var damageDirection = _projectile.HorizontalVelocity.normalized;
+            enemy.Damage(_settings.Damage, damageDirection);
+
+            var horizontalSpeed = Random.Range(_settings.ReboundHorizontalSpeed.start, _settings.ReboundHorizontalSpeed.end);
+            var verticalSpeed = Random.Range(_settings.ReboundVerticalSpeed.start, _settings.ReboundVerticalSpeed.end);
+
+            _projectileTrigger.enabled = false;
+            _projectile.GravityScale = _settings.ReboundGravity;
+            _projectile.SetVelocity(damageDirection * -horizontalSpeed, verticalSpeed);
+
+            _shadow.Visible = false;
+            
+            // choose rebound angle based on hit direction 
+            var angle = (damageDirection.x > 0) ? _settings.ReboundRotationAngle : -_settings.ReboundRotationAngle;
+            _view.Rotate(angle, _settings.ReboundRotationTime);
+            _view.FadeOut(_settings.ReboundFadeOutTime);
+        }
+
+        private void OnGroundHit()
+        {
+            Destroy(gameObject);
         }
         #endregion
     }
