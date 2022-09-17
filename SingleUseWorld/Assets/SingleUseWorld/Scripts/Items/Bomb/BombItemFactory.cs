99b3dc5747cbfac47ae6addca79ec69b49fe2969
SplineFox <89731027+SplineFox@users.noreply.github.com>
2022-09-17 18:21:18 +0900

    feat: Add BombItemFactory class

---
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Items/Bomb/BombItemFactory.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace SingleUseWorld
+{
+    [CreateAssetMenu(fileName = "BombItemFactorySO", menuName = "SingleUseWorld/Factories/Items/BombItem Factory SO")]
+    public class BombItemFactory : ScriptableFactory, IMonoFactory<Item>
+    {
+        #region Fields
+        [SerializeField] private Item _bombItemPrefab;
+        [SerializeField] private BombEntityFactory _bombEntityFactory;
+        #endregion
+
+        #region Public Methods
+        public Item Create()
+        {
+            var bombItem = CreateInstance<Item>(_bombItemPrefab);
+            bombItem.OnCreate(ItemType.Bomb, _bombEntityFactory);
+            return bombItem;
+        }
+        #endregion
+    }
+}
