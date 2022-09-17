e947a65876347c6413a13b6ebf52751d33e2b3f3
SplineFox <89731027+SplineFox@users.noreply.github.com>
2022-09-17 13:06:50 +0900

    feat: Add BowItemFactory class

---
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Items/Bow/BowItemFactory.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace SingleUseWorld
+{
+    [CreateAssetMenu(fileName = "BowItemFactorySO", menuName = "SingleUseWorld/Factories/Items/BowItem Factory SO")]
+    public class BowItemFactory : ScriptableFactory, IMonoFactory<Item>
+    {
+        #region Fields
+        [SerializeField] private Item _bowItemPrefab;
+        [SerializeField] private ArrowEntityFactory _arrowEntityFactory;
+        #endregion
+
+        #region Public Methods
+        public Item Create()
+        {
+            var bowItem = CreateInstance<Item>(_bowItemPrefab);
+            bowItem.OnCreate(ItemType.Bow, _arrowEntityFactory);
+            return bowItem;
+        }
+        #endregion
+    }
+}
