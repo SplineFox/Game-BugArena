e43cddca5039cb63608ded2c3c6f7c5ea3ea5cf7
SplineFox <89731027+SplineFox@users.noreply.github.com>
2022-09-23 08:58:32 +0900

    feat: Add PlayerSpeed class
    This class is responsible for applying enemy and carried item speed multipliers as well as setting the final movement speed.

---
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Player/PlayerSpeed.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEngine;
+
+namespace SingleUseWorld
+{
+    public sealed class PlayerSpeed
+    {
+        #region Nested Classes
+        [Serializable]
+        public class Settings
+        {
+            public float InitialSpeed = 4f;
+            public float MinSpeed = 1.5f;
+        }
+        #endregion
+
+        #region Fields
+        private float _speed;
+        private float _enemyFactor;
+        private float _itemFactor;
+
+        private Settings _settings;
+        private ProjectileMovement2D _movement;
+        #endregion
+
+        #region Properties
+        public float Value => _speed;
+        #endregion
+
+        #region Constructors
+        public PlayerSpeed(Settings settings, ProjectileMovement2D movement)
+        {
+            _settings = settings;
+            _movement = movement;
+            _speed = settings.InitialSpeed;
+
+            _enemyFactor = 1f;
+            _itemFactor = 1f;
+
+            _movement.SetSpeed(settings.InitialSpeed);
+        }
+        #endregion
+
+        #region Public Methods
+        public void SetEnemyFactor(float enemyFactor)
+        {
+            _enemyFactor = enemyFactor;
+            UpdateSpeed();
+        }
+
+        public void SetItemFactor(float itemFactor)
+        {
+            _itemFactor = itemFactor;
+            UpdateSpeed();
+        }
+
+        public void ResetEnemyFactor()
+        {
+            SetEnemyFactor(1f);
+        }
+
+        public void ResetItemFactor()
+        {
+            SetItemFactor(1f);
+        }
+        #endregion
+
+        #region Private Methods
+        private void UpdateSpeed()
+        {
+            var newSpeed = _settings.InitialSpeed * _enemyFactor * _itemFactor;
+            newSpeed = Math.Max(_settings.MinSpeed, newSpeed);
+
+            if (_speed == newSpeed)
+                return;
+
+            _speed = newSpeed;
+            _movement.SetSpeed(newSpeed);
+        }
+        #endregion
+    }
+}
