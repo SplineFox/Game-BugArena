19c881c7d8ffd3fb0ceb25de5b26c23d9b8bdf27
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-01-14 21:41:16 +1000

    feat: Add LevelBoundary class for marking spawn zone

---
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Core/Management/LevelBoundary.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace SingleUseWorld
+{
+    public class LevelBoundary
+    {
+        #region Fields
+        private BoxCollider2D _boxCollider;
+        #endregion
+
+        #region Constructors
+        public LevelBoundary(BoxCollider2D boxCollider)
+        {
+            _boxCollider = boxCollider;
+        }
+        #endregion
+
+        #region Public Methods
+        public Vector3 GetRandomPositionInside()
+        {
+            var position = new Vector3(
+                Random.Range(_boxCollider.bounds.min.x, _boxCollider.bounds.max.x),
+                Random.Range(_boxCollider.bounds.min.y, _boxCollider.bounds.max.y),
+                Random.Range(_boxCollider.bounds.min.z, _boxCollider.bounds.max.z));
+
+            return position;
+        }
+
+        public bool IsPositionInside(Vector3 position)
+        {
+            return _boxCollider.OverlapPoint(position);
+        }
+        #endregion
+    }
+}
