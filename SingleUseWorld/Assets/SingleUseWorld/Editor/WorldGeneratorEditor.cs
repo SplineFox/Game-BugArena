7005a9e04c921968849b478a533aeec478fcd01d
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-03-05 17:59:44 +1000

    feat: Add WorldGeneratorEditor class

---
+++ SingleUseWorld/Assets/SingleUseWorld/Editor/WorldGeneratorEditor.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace SingleUseWorld
+{
+    [CustomEditor(typeof(WorldGenerator))]
+    public class WorldGeneratorEditor : Editor
+    {
+        #region LifeCycle Methods
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var worldGenerator = (WorldGenerator)target;
+            if(GUILayout.Button("Generate world"))
+            {
+                worldGenerator.Generate();
+            }
+        }
+        #endregion
+    }
+}
