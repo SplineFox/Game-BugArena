adf37314c889b8892074985f4e3948747366a9c8
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-03-14 19:07:58 +1000

    feat: Add EffectAppearanceFactory class

---
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/VFX/EffectAppearanceFactory.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace SingleUseWorld
+{
+    public class EffectAppearanceFactory
+    {
+        private EffectAppearanceBuilder _appearance;
+
+        public EffectAppearanceFactory()
+        {
+            _appearance = new EffectAppearanceBuilder();
+        }
+
+        public EffectAppearance Create(EffectType effectType, Vector3 position)
+        {
+            _appearance.Reset();
+            _appearance.WithPosition(position);
+            switch (effectType)
+            {
+                case EffectType.StepDust:
+                case EffectType.PoofDust:
+                    _appearance.WithRandomRotation()
+                               .WithRandomVelocity(Vector3.right, 1f);
+                    break;
+                case EffectType.Smoke:
+                    _appearance.WithRandomOffset(2f)
+                               .WithRandomRotation()
+                               .WithRandomVelocity(Vector3.right, 1.25f)
+                               .WithRandomPlaybackTime(0.4f);
+                    break;
+                case EffectType.Blast:
+                    _appearance.WithRandomRotation();
+                    break;
+            }
+            return _appearance.Build();
+        }
+
+        public EffectAppearance Create(ComplexEffectType complexEffectType, Vector3 position)
+        {
+            _appearance.Reset();
+            _appearance.WithPosition(position)
+                       .WithRandomRotation()
+                       .WithRandomVelocity(Vector3.right, 1.25f);
+            return _appearance.Build();
+        }
+    }
+}
