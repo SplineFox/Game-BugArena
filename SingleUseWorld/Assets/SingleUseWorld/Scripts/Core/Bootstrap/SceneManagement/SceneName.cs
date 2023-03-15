df2f3268532d08166acd206f39461efc2ba65f7f
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-03-15 10:28:52 +1000

    feat: Add SceneLoader class

---
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Core/Bootstrap/SceneManagement/SceneName.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace SingleUseWorld
+{
+    public static class SceneName
+    {
+        public const string Bootstrap = "BootstrapScene";
+        public const string Arena = "ArenaScene";
+    }
+}
