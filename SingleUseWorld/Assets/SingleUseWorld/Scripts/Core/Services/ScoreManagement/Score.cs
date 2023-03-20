9957c0a97ccb936e48709464b88b3945a5b195ae
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-03-20 20:17:06 +1000

    chore: Move scripts related to Score to ScoreManagement folder

--- SingleUseWorld/Assets/SingleUseWorld/Scripts/Core/Services/ScoreManagement/Score.cs
+++ SingleUseWorld/Assets/SingleUseWorld/Scripts/Core/Services/ScoreManagement/Score.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 
 namespace SingleUseWorld
 {
+    [Serializable]
     public class Score
     {
         #region Fields
         private int _points = 0;
+        private int _pointsRecord = 0;
         #endregion
 
         #region Properties
         public int Points
         {
             get => _points;
+        }
+
+        public int Record
+        {
+            get => _pointsRecord;
         }
         #endregion
 
@@ -21,21 +28,29 @@ namespace SingleUseWorld
         #endregion
 
         #region Constructors
-        public Score(int initialPoints)
+        public Score()
         {
-            _points = initialPoints;
+            _points = 0;
+            _pointsRecord = 0;
+        }
+
+        public Score(int points, int pointsRecord)
+        {
+            _points = points;
+            _pointsRecord = pointsRecord;
         }
         #endregion
 
         #region Public Methods
         public void AddPoints(int points)
         {
+            if (points == 0)
+                return;
+
             _points += points;
+            _pointsRecord = Mathf.Max(_points, _pointsRecord);
             Changed.Invoke();
         }
-        #endregion
-
-        #region Private Methods
         #endregion
     }
 }
