8b0e3ea5ea0d25f2e867d8614298e386942c9471
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-04-03 10:00:04 +1000

    chore: Restructure Prefabs and Configs folders

--- BugArena/Assets/BugArena/Scripts/Core/Management/ConfigManagement/ConfigPath.cs
+++ BugArena/Assets/BugArena/Scripts/Core/Management/ConfigManagement/ConfigPath.cs
@@ -8,16 +8,16 @@ namespace BugArena
 
         public const string PlayerSettings      = "Configs/Player/PlayerSettings";
         public const string EnemySettings       = "Configs/Enemies/EnemySettings";
+
         public const string ItemSettings        = "Configs/Items/ItemSettings";
+        public const string BombItemSettings    = "Configs/Items/BombItemSettings";
+        public const string BowItemSettings     = "Configs/Items/BowItemSettings";
+        public const string SkullItemSettings   = "Configs/Items/SkullItemSettings";
+        public const string SwordItemSettings   = "Configs/Items/SwordItemSettings";
 
-        public const string BombItemSettings    = "Configs/Items/Bomb/BombItemSettings";
-        public const string BowItemSettings     = "Configs/Items/Bow/BowItemSettings";
-        public const string SkullItemSettings   = "Configs/Items/Skull/SkullItemSettings";
-        public const string SwordItemSettings   = "Configs/Items/Sword/SwordItemSettings";
-
-        public const string BombEntitySettings  = "Configs/Items/Bomb/BombEntitySettings";
-        public const string ArrowEntitySettings = "Configs/Items/Bow/ArrowEntitySettings";
-        public const string SkullEntitySettings = "Configs/Items/Skull/SkullEntitySettings";
-        public const string SwordEntitySettings = "Configs/Items/Sword/SwordEntitySettings";
+        public const string BombEntitySettings  = "Configs/Entities/BombEntitySettings";
+        public const string ArrowEntitySettings = "Configs/Entities/ArrowEntitySettings";
+        public const string SkullEntitySettings = "Configs/Entities/SkullEntitySettings";
+        public const string SwordEntitySettings = "Configs/Entities/SwordEntitySettings";
     }
 }
--- BugArena/Assets/BugArena/Scripts/Core/Management/PrefabManagement/PrefabPath.cs
+++ BugArena/Assets/BugArena/Scripts/Core/Management/PrefabManagement/PrefabPath.cs
@@ -4,31 +4,31 @@ namespace BugArena
 {
     public static class PrefabPath
     {
-        public const string Player          = "Prefabs/Bug";
-        public const string Enemy           = "Prefabs/Mite";
+        public const string Player          = "Prefabs/Player/Bug";
+        public const string Enemy           = "Prefabs/Enemies/Mite";
 
-        public const string StepDust        = "Prefabs/StepDust";
-        public const string PoofDust        = "Prefabs/PoofDust";
-        public const string Smoke           = "Prefabs/Smoke";
-        public const string Blast           = "Prefabs/Blast";
+        public const string StepDust        = "Prefabs/Effects/StepDust";
+        public const string PoofDust        = "Prefabs/Effects/PoofDust";
+        public const string Smoke           = "Prefabs/Effects/Smoke";
+        public const string Blast           = "Prefabs/Effects/Blast";
 
-        public const string BombItem        = "Prefabs/BombItem";
-        public const string BowItem         = "Prefabs/BowItem";
-        public const string SkullItem       = "Prefabs/SkullItem";
-        public const string SwordItem       = "Prefabs/SwordItem";
+        public const string BombItem        = "Prefabs/Items/BombItem";
+        public const string BowItem         = "Prefabs/Items/BowItem";
+        public const string SkullItem       = "Prefabs/Items/SkullItem";
+        public const string SwordItem       = "Prefabs/Items/SwordItem";
 
-        public const string BombEntity      = "Prefabs/BombEntity";
-        public const string ArrowEntity     = "Prefabs/ArrowEntity";
-        public const string SkullEntity     = "Prefabs/SkullEntity";
-        public const string SwordEntity     = "Prefabs/SwordEntity";
+        public const string BombEntity      = "Prefabs/Entities/BombEntity";
+        public const string ArrowEntity     = "Prefabs/Entities/ArrowEntity";
+        public const string SkullEntity     = "Prefabs/Entities/SkullEntity";
+        public const string SwordEntity     = "Prefabs/Entities/SwordEntity";
 
-        public const string ArenaCamera     = "Prefabs/MainCamera";
-        public const string ArenaHud        = "Prefabs/ArenaHud";
+        public const string ArenaCamera     = "Prefabs/Arena/MainCamera";
+        public const string ArenaHud        = "Prefabs/Arena/ArenaHud";
 
-        public const string MenuRoot        = "Prefabs/MenuRoot";
-        public const string MenuEventSystem = "Prefabs/MenuEventSystem";
-        public const string MainMenu        = "Prefabs/MainMenu";
-        public const string PauseMenu       = "Prefabs/PauseMenu";
-        public const string RestartMenu     = "Prefabs/RestartMenu";
+        public const string MenuRoot        = "Prefabs/Menus/MenuRoot";
+        public const string MenuEventSystem = "Prefabs/Menus/MenuEventSystem";
+        public const string MainMenu        = "Prefabs/Menus/MainMenu";
+        public const string PauseMenu       = "Prefabs/Menus/PauseMenu";
+        public const string RestartMenu     = "Prefabs/Menus/RestartMenu";
     }   
 }
